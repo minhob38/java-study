8d936d2719e13f1b6650fd16c51ca2436ff0646b
kwon, minho <minhob38@gmail.com>
2022-01-16 00:24:07 +0900

    update class

--- src/Class.java
+++ src/Class.java
@@ -11,7 +11,7 @@ public class Class {
           myCar = new Car("1.4t", "blue");
           System.out.println("myCar의 engine/color는 " + myCar.engine + "/" + myCar.color);
           System.out.println("myCar의 wheel은" + myCar.wheel);
-          System.out.println("Car의 wheel은" + myCar.wheel);
+          System.out.println("Car의 wheel은" + Car.wheel);
           Car.blow();
           myCar.blow();
           myCar.info();
@@ -20,34 +20,34 @@ public class Class {
 }
 
 /*
-=== class ===
+=== class 만들기 ===
 class 변수
-- 멤버변수: 인스턴스변수 (인스턴스에서 접근이 가능합니다.)
-- 멤버변수: static(class)변수 (인스턴스를 생성하지 않고 클래스에서 접근이 가능하며, 인스턴스에서도 접근이 가능합니다.)
+- 멤버변수: instance 변수 (instance에서 접근이 가능합니다.)
+- 멤버변수: static(class)변수 (instance/class에서 접근이 가능합니다.)
 - 지역변수:
 
-class 메소드
-- staic(class) 메소드: 인스턴스를 생성하지 않고 클래스에서 호출이 가능하며, 인스턴스에서도 호출이 가능합니다.)
-- instance 메소드: 인스턴스에서 호출이 가능합니다.
+class mehod
+- static(class) method: instance/class에서 호출이 가능합니다.
+- instance method: instance에서 호출이 가능합니다.
 
 constructor(생성자)
-- 생성자를 통해 인스턴스를 초기화 할 수 있습니다.
+- 생성자를 통해 instance를 초기화 할 수 있습니다.
 - 생성자의 이름은 class와 같아야하며, 반환값은 없습니다.
 
 overloading
-- 하나의 클래스 안에서 같은 이름으로 여러개의 메소드를 만들 수 있습니다.
-- 메소드의 매개변수 개수 또는 자료형이 달라야합니다.
+- 하나의 class 안에서 같은 이름으로 여러개의 method를 만들 수 있습니다.
+- method의 매개변수 개수 또는 자료형이 달라야합니다.
 
 this / this()
 - this와 this()는 유사해보이지만 다른것입니다.
-- this는 인스턴스 스스로를 가리킵니다.
+- this는 instance 스스로를 가리킵니다.
 - this()는 class 생성자입니다.
 */
 class Car {
     static int wheel = 4; // static(class) 변수
     static String fuel = "gasoline"; // static(class) 변수
-    String engine; // 인스턴스변수
-    String color; // 인스턴스변수
+    String engine; // instance 변수
+    String color; // instance 변수
 
     // 생성자
     Car() {
@@ -60,11 +60,11 @@ class Car {
         color = c;
     }
 
-    static void blow() { // static(class) 메소드 (인스턴스(this)에 접근할 수 없습니다.)
+    static void blow() { // static(class) method (instance(this)에 접근할 수 없습니다.)
         System.out.println("빵빵");
     }
 
-    void info() { // static(class) 메소드
+    void info() { // static(class) method
         System.out.println("해당 차량은 " + this.wheel + " " + this.fuel + " " +  this.engine + " " + this.color + " " + "입니다.");
     }
 
