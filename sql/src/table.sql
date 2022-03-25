89229e1c5de01c08eb0ed5ca1c069ba31f26b3f1
minho <minhob38@gmail.com>
2022-03-25 12:05:33 +0900

    Update table.sql

--- sql/src/table.sql
+++ sql/src/table.sql
@@ -7,3 +7,21 @@
 
 -- [table column 추가하기]
 -- alter table [talbe 이름] add column [column 이름] [type] [constraint]
+
+-- *** [table 만들기] ***
+-- create table로 table을 만듭니다.
+CREATE TABLE inapp.verification_messages (
+	id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ,
+	phone_number varchar NOT NULL,
+	verification_code varchar NOT NULL,
+	is_verified bool NOT NULL DEFAULT false,
+	is_expired bool NOT NULL DEFAULT false,
+	message_count smallint NOT NULL DEFAULT 0,
+	created_at date NOT NULL DEFAULT now(),
+	updated_at date,
+	CONSTRAINT verification_messages_pk PRIMARY KEY (id)
+);
+
+-- pk 이름 [table 이름]_pkey
+-- foreign key 이름
+-- [table 이름]_[foreign key 이름]_fkey
